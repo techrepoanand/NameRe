c7a9f574b218a5af214090db00c8659f6cb76bd4
techrepoanand <166824753+techrepoanand@users.noreply.github.com>
2024-04-19 14:47:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ DB_URL = os.environ.get("DB_URL","")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
-START_PIC = os.environ.get("START_PIC", "https://graph.org/Rename-Bot-01-15")
+START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/bf9d252e374bff990c954.png")
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6777494089').split()]
 
